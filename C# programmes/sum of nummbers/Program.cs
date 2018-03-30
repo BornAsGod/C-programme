59d1c0ec5f12278dedce68f27bceb3ddbf85d2df
BornAsGod <37935989+BornAsGod@users.noreply.github.com>
2018-03-30 20:19:27 +0300

    Add files via upload

---
+++ C# programmes/sum of nummbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int sum = 0;
+            Console.Write("numbers:");
+            int num = int.Parse(Console.ReadLine());
+            for (int n = 1; n <= num; n++)
+            {
+                Console.Write("number{0}-",n);
+                int b = int.Parse(Console.ReadLine());
+                sum = sum + b;
+
+            }
+
+            Console.WriteLine("the sum is {0}",sum);
+        }
+    }
+}
